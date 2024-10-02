5537e9d1e9339d1fc822958b2941bfecf17005e8
phewson16 <peterhewson16@gmail.com>
2024-10-02 11:53:46 -0400

    final commit

--- index.js
+++ index.js
@@ -6,11 +6,16 @@
 // Properties
 // Name, Age, Gender (generic properties)
 // isWalking, isSprinting, isCrouched (properties tied to animations)
+// MovementSpeed, jumpHeight (properties tied to motion)
 
 // Events
-// Click 'change arrow type'
+// Click 'change arrow type' button
+// Click sprint key
+// Right trigger hold to ads, left trigger to fire arrow
 
 
 // Methods
 // Current arrow type changes
+// Change from walking to running (and animation)
+// Arrow is fired in direction of crosshair
 
