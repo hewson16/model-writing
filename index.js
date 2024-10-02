c1e395f891dfeaa2f1244e0f422660592b8f2afb
phewson16 <peterhewson16@gmail.com>
2024-10-02 10:27:15 -0400

    update w/ properties

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// comment
+// Model Writing
+
+// Character from Favorite Video Game
+// Character: Aloy from Horizon Zero Dawn
+
+// Properties
+// Name, Age, Gender (generic properties)
+// isWalking, isSprinting, isCrouched (properties tied to animations)
+
+// Events
+
+
+// Methods
+
